039a9585ad8bec0da331655dd99c2d48603e9ee6
nicooliver <nicooliver@outlook.com>
2023-01-19 10:20:09 +0100

    update

--- grease-ranker-api/Program.cs
+++ grease-ranker-api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.d
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
